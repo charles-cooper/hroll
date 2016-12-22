0303302baff0ba81c4812a51daf0f7efa7da49c5
Charles Cooper <cooper.charles.m@gmail.com>
2016-12-21 20:11:19 -0500

    Implement primitive benchmark

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,15 @@
 module Main where
-import Deque
+import Deque as D
 import Covariance
+import Data.Time.Clock
 import Data.IORef
+import Control.Monad
+import System.Random
+import qualified Data.Vector.Unboxed as U
+import qualified Data.List as L
+import Control.DeepSeq
+import Control.Exception
+import Statistics.Sample
 
 summer :: Deque Int
 summer = deque (+)
@@ -23,33 +31,46 @@ popTrace xs = do
 
 showCov = show . getCovariance
 
+-- Generates a lazy list of windows
+windows :: Int -> U.Vector Double -> U.Vector Double -> [U.Vector (Double, Double)]
+windows k xs ys = let
+  zs = pair xs ys
+  in flip L.unfoldr zs $ \vec -> if U.length vec >= k
+    then Just (U.take k vec, U.drop 1 vec)
+    else Nothing
+
+queues :: Int -> U.Vector Double -> U.Vector Double -> [Deque Covariance]
+queues k xs ys = let
+  zs = uncurry mkCovariance <$> U.toList (pair xs ys)
+  pushPop x d = case D.pop (D.push x d) of
+    Nothing -> error "Impossible situation: push resulted in empty queue"
+    Just d' -> snd d'
+  go d x = if D.size d == k
+    then pushPop x d
+    else push x d
+  in dropWhile (\d -> D.size d < k) $ L.scanl' go cov zs
+
+time :: String -> IO a -> IO a
+time msg action = do
+  t0 <- getCurrentTime
+  r  <- action
+  t1 <- getCurrentTime
+  putStrLn $ msg ++ " took " ++ show (t1 `diffUTCTime` t0)
+  return r
+
+covariance' :: Deque Covariance -> Double
+covariance' = maybe (0/0) getCovariance . runFold
+
 -- simple test suite
 main :: IO ()
 main = do
-  x <- newIORef summer
-  pushTrace 1 x -- 1
-  pushTrace 2 x -- 3
-  pushTrace 3 x -- 6
-  popTrace x    -- 5 [2,3]
-  popTrace x    -- 3 [3]
-  pushTrace 2 x -- 5 [3,2]
-  pushTrace 3 x -- 8 [3,2,3]
-  popTrace x    -- 5 [2,3]
-  popTrace x    -- 3 [3]
-  pushTrace 1 x -- 4 [3,1]
-  popTrace x    -- 1 [1]
-  popTrace x    -- Nothing
-
-  y <- newIORef cov
-  -- check against numpy
-  pushTrace (mkCovariance 1 1) y     -- [1], [1]
-  pushTrace (mkCovariance 1 1.5) y   -- [1,1], [1.5,1]
-  pushTrace (mkCovariance 0.9 0.5) y -- [0.9,1,1], [0.5,1.5,1]
-  pushTrace (mkCovariance 0.8 0.2) y -- [0.8,0.9,1,1], [0.2,0.5,1.5,1]
-  popTrace y                         -- [0.8,0.9,1], [0.2,0.5,1.5]
-  popTrace y                         -- [0.8,0.9], [0.2,0.5]
-  pushTrace (mkCovariance 0.0 0.0) y -- [0.0,0.8,0.9], [0.0,0.2,0.5]
-  popTrace y                         -- [0.0,0.8], [0.0,0.2]
-  popTrace y                         -- [0.0], [0.0]
-  popTrace y                         -- [], []
+  -- Find the rolling covariance of two randomly generated, million element vectors
+  xs <- evaluate . force =<< U.fromList <$> replicateM 1000000 randomIO
+  ys <- evaluate . force =<< U.fromList <$> replicateM 1000000 randomIO
+  time "Naive 100"    $ print $ sum $ covariance  <$> windows 100 xs ys
+  time "Window 100"   $ print $ sum $ covariance' <$> queues  100 xs ys
+  time "Naive 1000"   $ print $ sum $ covariance  <$> windows 1000 xs ys
+  time "Window 1000"  $ print $ sum $ covariance' <$> queues  1000 xs ys
+  time "Naive 10000"  $ print $ sum $ covariance  <$> windows 10000 xs ys
+  time "Window 10000" $ print $ sum $ covariance' <$> queues  10000 xs ys
 
